77239da9b5b1956f093a199d0dbec4238353029e
alina 颖思 <106563068+charcoalyy@users.noreply.github.com>
2024-09-07 17:14:01 -0400

    update about

--- src/components/constants/about.ts
+++ src/components/constants/about.ts
@@ -1,5 +1,5 @@
 export const about = {
-  bio: "Software engineering student who lives for building cool things and meeting cooler people. Seeking Summer 2025 internships.",
+  bio: "Software engineering student who likes building cool things and meeting cooler people. Seeking Summer 2025 internships.",
   current:
     "Currently a Software Engineer Intern at Wealthsimple and Design Co-Lead at McMaster Design League. Otherwise  found writing, drawing, gymming, or watching anime ₍ᐢ. .ᐢ₎ ₊˚⊹♡",
   professional: ["Software Engineer Intern @Wealthsimple"],
