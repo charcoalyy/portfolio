97f3bcea666f21001cfcba32f6e9610d9aa26218
milo <106563068+charcoalyy@users.noreply.github.com>
2024-11-24 17:44:15 -0500

    minor grammar fix

--- src/components/constants/about.ts
+++ src/components/constants/about.ts
@@ -1,5 +1,5 @@
 export const about = {
   bio: "Software engineering student who likes building cool things and meeting cool people. Always hungry for something new. Seeking Summer 2025 internships.",
   current:
-    "Previously a Software Engineer Intern at Wealthsimple. Currently a Design Lead at McMaster Design League. Otherwise found drawing digital gouache portraits, reading narrative nonfiction, or watching The Good Place ꒰ᐢ. .ᐢ꒱₊˚⊹",
+    "Previously Software Engineer Intern at Wealthsimple. Currently Design Lead at McMaster Design League. Otherwise found drawing digital gouache portraits, reading narrative nonfiction, or watching The Good Place ꒰ᐢ. .ᐢ꒱₊˚⊹",
 }
