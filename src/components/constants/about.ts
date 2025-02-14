8042286542794eb167c4a77ed9e4e7d463c9e318
milo <106563068+charcoalyy@users.noreply.github.com>
2025-02-13 22:03:49 -0500

    new update

--- src/components/constants/about.ts
+++ src/components/constants/about.ts
@@ -1,5 +1,5 @@
 export const about = {
-  bio: "Software engineering student who likes building cool things and meeting cool people. Always hungry for something new. Seeking Summer 2025 internships.",
+  bio: "Software engineering student who likes building cool things and meeting cool people. Always hungry for something new. Incoming at MongoDB for Summer 2025!",
   current:
-    "Previously Software Engineer Intern at Wealthsimple. Currently Design Lead at McMaster Design League. Otherwise found drawing digital gouache portraits, reading narrative nonfiction, or watching The Good Place ꒰ᐢ. .ᐢ꒱₊˚⊹",
+    "Previously Software Engineer Intern at Wealthsimple. Currently Design Lead at McMaster Design League. Otherwise found drawing digital gouache portraits, reading narrative nonfiction, or watching a show everyone was raving over several years ago ꒰ᐢ. .ᐢ꒱₊˚⊹",
 }
