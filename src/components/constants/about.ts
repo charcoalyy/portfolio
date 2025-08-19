9ec4667432a657827437bba83d258a2999c84210
alina 颖思 <106563068+charcoalyy@users.noreply.github.com>
2025-08-19 15:49:08 -0400

    updates to about (concision)

--- src/components/constants/about.ts
+++ src/components/constants/about.ts
@@ -1,5 +1,5 @@
 export const about = {
-  bio: "Software engineering student. Previously an intern at MongoDB and Wealthsimple. Currently interested in reliability and devprod - always hungry for something new!",
+  bio: "Software engineering student interested in reliability and devprod. Previously interned at MongoDB and Wealthsimple. Always hungry for something new!",
   current:
     "For fun, leading the visual design team at McMaster Design League. Otherwise found colouring outside the lines, writing short stories, or bruising myself learning to climb ꒰ᐢ. .ᐢ꒱₊˚⊹",
 }
