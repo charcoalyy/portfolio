c5ba3a0662eec9cf60e23475865d74de73b86490
Alina Zeng <thealinamachine@192.168.1.190>
2026-04-27 21:59:39 -0400

    color update

--- src/components/constants/about.ts
+++ src/components/constants/about.ts
@@ -1,5 +1,5 @@
 export const about = {
-  bio: "Software engineering student interested in reliability engineering and distributed systems performance. Previously spent summers building developer-facing tools at MongoDB and Wealthsimple. Always hunting for something new!",
+  bio: "Software engineering student interested in reliability engineering and distributed systems performance. Spent a couple summers at MongoDB and Wealthsimple working on developer tooling & infrastructure; always hunting for something new!",
   current:
-    "For fun, leading the creative team at McMaster Design League. Otherwise found colouring outside the lines, writing short stories, or getting a new bruise learning to climb ꒰ᐢ. .ᐢ꒱₊˚⊹",
+    "For fun, managing the creative teams at McMaster Design League. Otherwise found: colouring outside the lines, writing short stories, getting new bruises learning to climb~ ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧",
 }
