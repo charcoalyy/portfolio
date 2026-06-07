c4cc44ce5cb85c3afd755c00ffa4a80156eb9e82
Alina Zeng <alina.yzeng@gmail.com>
2026-06-06 22:04:24 -0400

    about update

--- src/components/constants/about.ts
+++ src/components/constants/about.ts
@@ -1,5 +1,5 @@
 export const about = {
-  bio: "Software engineering student interested in reliability engineering and distributed systems performance. Spent a couple summers at MongoDB and Wealthsimple working on developer tooling & infrastructure; always hunting for something new!",
+  bio: "Software engineering student interested in reliability engineering and distributed systems performance. Spending this summer at MongoDB on core server (networking & observability). Always hunting for something new!",
   current:
     "For fun, managing the creative teams at McMaster Design League. Otherwise found: colouring outside the lines, writing short stories, getting new bruises learning to climb~ ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧",
 }
